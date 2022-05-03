1433a93640acfda287fed3cc2b569d96f7180250
Zacharie KOHL <Wateranlt@alexis.firhold@yahoo.com>
2022-05-03 10:05:32 +0200

    Setting some goals

--- Sorting_Algorithms/Sorting Algorithms.h
+++ Sorting_Algorithms/Sorting Algorithms.h
@@ -6,5 +6,17 @@ public:
 	static void printList(int array[], size_t n);
 	static void swap(int& var1, int& var2);
 	static void insertionSort(int array[], size_t n);
-
+	//Selection Sort
+	//QuickSort
+	//Merge Sort
+	//Heap Sort
+	//Radix Sort
+	//std::sort
+	//std::stable_sort
+	//shell sort
+	//Bubble sort
+	//Cocktail Shaker Sort
+	//Gnome Sort
+	//Bitonic Sort
+	//Bogo Sort
 };
